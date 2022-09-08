ecbbb5c9a3ba23965c8affaa73116b71af094b0a
xdgonex <bendezucortezjoseluis@gmail.com>
2022-09-08 02:30:58 -0500

    Agregando valores al Select

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 //variables
 
 const resultado = document.querySelector('#resultado');
+const select = document.querySelector('#year');
+const max = new Date().getFullYear(); //Permite tener el año actual
+const min = max - 10;
 
 
+document.addEventListener('DOMContentLoaded',()=>{
 
-document.addEventListener('DOMContentLoaded',mostrarAutos);
+    mostrarAutos(); //Muestra todos los autos en RESULTADOS
+
+    llenarSelect(); //Llenara el select de Año
+
+});
 
 //funciones
 
@@ -19,4 +27,18 @@ function mostrarAutos()
 
         resultado.appendChild(autoHTML);
     });
+}
+
+function llenarSelect()
+{
+    //Generamos una lista del año actual hasta -10 años atras
+    for(let i = max; i>=min; i--)
+    {
+        //Creamos un elemento OPTION
+        const option = document.createElement('option');
+        option.value = i;   //le dimos el valor a cada option de i
+        option.textContent = i; //agregamos texto
+
+        select.appendChild(option); //agregamos un hijo al elemento select.
+    }
 }
